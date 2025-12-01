f5ac88f9cf8461e6c6b557a6eeea3a62b836828f
suresh <s3jmpc@gmail.com>
2025-12-02 01:03:33 +0530

    Connected database to cloud PostgreSQL using env

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,15 +1,15 @@
-// app/contact/page.js
-import ContactForm from "../../components/ContactForm"
+  // app/contact/page.js
+  import ContactForm from "../../components/ContactForm"
 
-export default function ContactPage() {
-  return (
-    <section className="max-w-6xl mx-auto px-4 py-16">
-      <h1 className="text-3xl font-bold text-yellow-900">Contact Us</h1>
-      <p className="mt-3 text-gray-700">Have a question or bulk order? Drop a message.</p>
+  export default function ContactPage() {
+    return (
+      <section className="max-w-6xl mx-auto px-4 py-16">
+        <h1 className="text-3xl font-bold text-yellow-900">Contact Us</h1>
+        <p className="mt-3 text-gray-700">Have a question or bulk order? Drop a message.</p>
 
-      <div className="mt-8">
-        <ContactForm />
-      </div>
-    </section>
-  )
-}
+        <div className="mt-8">
+          <ContactForm />
+        </div>
+      </section>
+    )
+  }
